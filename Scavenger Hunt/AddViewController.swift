4ccd3781e663a6b626c39b0428a7491b55dcd0ed
Cameron <cameron.farzaneh@gmail.com>
2016-02-04 21:32:55 -0500

    Embedded in NavigationViewController and added AddViewController

--- Scavenger Hunt/AddViewController.swift
+++ Scavenger Hunt/AddViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AddViewController.swift
 //  Scavenger Hunt
 //
 //  Created by Cameron Farzaneh on 2/4/16.
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AddViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func cancelButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
