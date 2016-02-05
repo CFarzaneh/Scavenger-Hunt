2332cfdc96af5a7ab431406b26031e8931fad6ce
Cameron <cameron.farzaneh@gmail.com>
2016-02-04 20:28:45 -0500

    Added ListViewController

---
+++ Scavenger Hunt/ListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Cameron Farzaneh on 2/4/16.
+//  Copyright © 2016 CFarzaneh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+}
